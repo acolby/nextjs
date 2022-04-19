fa81e054e8fd55ced1d634eeaa42c4878946674d
Aaron Colby <acolby@live.com>
2022-04-19 13:57:58 -0700

    starter

---
+++ src/Comps/hello/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {};
+
+export const Comps_hello = (props: Props) => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
